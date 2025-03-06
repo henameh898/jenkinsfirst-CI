772101ace518c060cd695dbaa9fe3d0e57ae7604
mehedi hasan <mhasanwk@gmail.com>
2025-03-05 21:48:56 -0500

    manual build

--- src/CODEFORCI/codeforci.java
+++ src/CODEFORCI/codeforci.java
@@ -8,6 +8,7 @@ System.out.println("jenkins firstproject for CI");
 System.out.println(20-10);
 System.out.println("jenkins build");
 System.out.println("trigger build for web");
+System.out.println("manual build");
 	}
 
 }
