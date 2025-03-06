c2b6b017379e3cda1cd3ac1800062d67ac96f528
mehedi hasan <mhasanwk@gmail.com>
2025-03-05 20:44:00 -0500

    number added

--- src/CODEFORCI/codeforci.java
+++ src/CODEFORCI/codeforci.java
@@ -4,7 +4,8 @@ public class codeforci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("jenkins firstproject for CI");
+System.out.println(20-10);
 	}
 
 }
